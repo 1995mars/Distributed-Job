c65a588efee19d938c754ffebb9e366fd94448b7
Mars <51732119+1995mars@users.noreply.github.com>
2025-11-15 19:45:59 +0700

    setup spring boot project

---
+++ job_service/src/main/java/org/mars/OneCvApplication.java
@@ -0,0 +1,13 @@
+package com.mars.OneCV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneCvApplication.class, args);
+	}
+
+}
